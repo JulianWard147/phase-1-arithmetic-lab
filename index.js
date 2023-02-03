ed47c92e749828be70f2a14c18d134b162bd109e
Julian Ward <wardjulianm@gmail.com>
2023-02-03 14:39:44 -0700

    Finishing lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+let num1 = 2
+let num2 = 31
+let num3 = 9
+let num4 = 5
+let num5 = 20
+
+
+let multiply = num1 * num2
+let random = Math.ceil(Math.random())
+let mod = num3 % num4
+let max = Math.max(num1, num3, num4, num5)
